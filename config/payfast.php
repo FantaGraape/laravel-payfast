dd8753e69313cd8d2534cec51b5fe8c311759e24
Timothy <timothy@resolutum.co.za>
2022-06-17 17:05:22 +0200

    Add ENV variable

--- config/payfast.php
+++ config/payfast.php
@@ -1,5 +1,9 @@
 <?php
 // config for EllisSystems/LaravelPayFast
 return [
-
+    'merchant_id' => env('PAYFAST_MERCHANT_ID', ''),
+    'merchant_key' => env('PAYFAST_MERCHANT_KEY', ''),
+    'passphrase' => env('PAYFAST_PASSPHRASE'),
+    'testmode' => env('PAYFAST_TESTMODE', true),
+    'proxy' => env('PAYFAST_PROXY'),
 ];
