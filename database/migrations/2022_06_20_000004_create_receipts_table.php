2ab83ab2733b750701c7d9eee5251f5f156498b4
Timothy <timothy@resolutum.co.za>
2022-06-21 09:03:44 +0200

    WIP

---
+++ database/migrations/2022_06_20_000004_create_receipts_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateReceiptsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('receipts', function (Blueprint $table) {
+            $table->id();
+            $table->string('payfast_token')->nullable()->index();
+            $table->string('merchant_payment_id')->nullable();
+            $table->string('payfast_payment_id')->unique();
+            $table->string('item_name');
+            $table->string('item_description')->nullable();
+            $table->string('amount_gross');
+            $table->string('amount_fee');
+            $table->string('amount_net');
+            $table->string('billable_type')->nullable();
+            $table->unsignedBigInteger('billable_id')->nullable();                        
+            $table->string('order_id')->nullable();
+            $table->timestamp('paid_at');
+            $table->timestamps();
+
+            $table->index(['billable_id', 'billable_type']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('receipts');
+    }
+}
